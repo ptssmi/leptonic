a88089ef00cc8e317a2ce5f21ed8a356145f5d7f
Preston Smith <ptssmith2@gmail.com>
2020-10-26 17:20:43 -0400

    trying a different packet

--- examples/telemetry.c
+++ examples/telemetry.c
@@ -84,7 +84,7 @@ int main(int argc, char *argv[])
   //log_info("VoSPI stream synchronised");
 
   // Parse the telemetry data
-  telemetry_data_t data = parse_telemetry_packet(&(frame.segments[0].packets[62])); //reads A register
+  telemetry_data_t data = parse_telemetry_packet(&(frame.segments[2].packets[0])); //reads A register
   // telemetry_data_t data = parse_telemetry_packet(&(frame.segments[1].packets[0])); //reads B register 
   // telemetry_data_t data = parse_telemetry_packet(&(frame.segments[2].packets[0])); //reads C register
 
