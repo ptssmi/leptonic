4e286dcb201048b19b654e1d9747ae0f2699fa52
Preston Smith <ptssmith2@gmail.com>
2020-10-27 15:22:09 -0400

    switching to double type

--- examples/telemetry.c
+++ examples/telemetry.c
@@ -133,7 +133,7 @@ int main(int argc, char *argv[])
   
 }
 
-int tempconverter(int tempinkelvin){
+double tempconverter(int tempinkelvin){
   double result;
   result = (((tempinkelvin/100) - 273.15) * 1.8) + 32;
   return result;
