335846cbd1640b33a4d740490e51f5e996c60a62
Preston Smith <ptssmith2@gmail.com>
2020-10-28 16:49:36 -0400

    forogot uint16

--- include/api/cci.h
+++ include/api/cci.h
@@ -89,7 +89,7 @@ uint32_t cci_get_agc_enable_state(int fd);
 
 
 // Module: Spotmeter //
-void cci_set_spotmeter_coordinates(int fd, cci_spotmeter startrow,uint16_t endrow, uint16_t startcol, uint16_t endcol);
+void cci_set_spotmeter_coordinates(int fd, uint16_t startrow,uint16_t endrow, uint16_t startcol, uint16_t endcol);
 uint32_t cci_get_spotmeter_coordinates(int fd);
 
 #endif /* CCI_H */
