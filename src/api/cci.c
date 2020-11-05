a395edb7a1a46c869ed461036360acce532c0f8d
Preston Smith <ptssmith2@gmail.com>
2020-11-05 17:03:22 -0500

    new

--- src/api/cci.c
+++ src/api/cci.c
@@ -229,17 +229,11 @@ void cci_set_spotmeter_coordinates(int fd, uint16_t startrow, uint16_t endrow, u
 {
   WAIT_FOR_BUSY_DEASSERT()
   cci_write_register(fd, CCI_REG_DATA_0, startrow);
-  printf("Pass1: %d:",startrow);
   cci_write_register(fd, CCI_REG_DATA_0 + CCI_WORD_LENGTH, endrow);
-  printf("Pass2: %d:",endrow);
   cci_write_register(fd, CCI_REG_DATA_0 + CCI_WORD_LENGTH + CCI_WORD_LENGTH, startcol);
-  printf("Pass3: %d:",startcol);
   cci_write_register(fd, CCI_REG_DATA_0 + CCI_WORD_LENGTH + CCI_WORD_LENGTH + CCI_WORD_LENGTH, endcol);
-  printf("Pass4: %d:",endcol);
-  cci_write_register(fd, CCI_REG_COMMAND, CCI_CMD_SYS_GET_SPOTMETER_REGION);
-  printf("Pass 5");
+  cci_write_register(fd, CCI_REG_COMMAND, CCI_CMD_SYS_SET_SPOTMETER_REGION);
   cci_write_register(fd, CCI_REG_DATA_LENGTH, 4);
-  printf("Pass 6");
   WAIT_FOR_BUSY_DEASSERT()
 }
 
