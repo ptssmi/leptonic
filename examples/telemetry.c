304176f372cfdf45e9f309d6b633f27bda97c5b8
Preston Smith <ptssmith2@gmail.com>
2020-11-05 23:54:26 -0500

    Update telemetry.c

--- examples/telemetry.c
+++ examples/telemetry.c
@@ -258,10 +258,15 @@ int main(int argc, char *argv[])
   }
   fclose(fp);
 
+  Py_Initialize();
+  PyRun_SimpleString("import sys");
+  PyRun_SimpleString("sys.path.append('/home/pi/ThermalRead/leptonic/bin/examples')");
   PyObject *mod = PyImport_ImportModule("test");
   PyObject *var1Py = PyObject_GetAttrString(mod, "x");
-  int c_var1 = PyLong_AsLong(var1Py);
-  printf("var1 with C: %d\n", c_var1);
+  int x = PyInt_AsLong(var1Py);
+  printf("var1=%d ; var1==NULL: %d\n", var1Int, var1Py==NULL);
+  Py_XDECREF(var1Py);
+  Py_Finalize();
 
 //  }
 
