ea6977396c69a674cfd5ba2c928e6ef872e155d5
Preston Smith <ptssmith2@gmail.com>
2020-11-08 21:42:24 -0500

    Update dataimport.py

--- examples/dataimport.py
+++ examples/dataimport.py
@@ -2,6 +2,9 @@
 with open("output.txt") as fp:
     for i, line in enumerate(fp):
         if i == 25:
-            print(line)
+            if float(line) < 100:
+                print("No fever detected.")
+            else:
+                print("Fever detected.")
 
     
